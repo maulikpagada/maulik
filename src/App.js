59a461742aac7f11244a75f9f0f876d6d74dbfe0
maulik <pagadamaulik4@gmail.com>
2023-02-20 16:06:54 +0530

    maulik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import employee from './Admin/container/employee';
+import Layout from './Admin/component/Layout'
+import Contact from './Admin/container/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <Switch>
+          {/* <Route exact path="/empolyee" component={employee} /> */}
+          <Route exact path="/Contact" component={Contact} />
+        </Switch>
+      </Layout>
+    </>
   );
 }
 
